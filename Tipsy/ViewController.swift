0ad52e7068afe08801189faa4b5da7938d91433c
Manvesh <manvesh@Manveshs-MacBook-Pro.local>
2014-08-19 23:52:59 -0700

    first commit

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+  @IBOutlet weak var TipInfoLabel: UILabel!
+  
+  @IBOutlet weak var TipLabel: UILabel!
+  
+  @IBOutlet weak var TotalLabel: UILabel!
+  
+  @IBOutlet weak var BillAmount: UITextField!
+
+  @IBOutlet weak var TipSlider: UISlider!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    BillAmount.text = ""
+    TipInfoLabel.text = "Tip @ \(lroundf(TipSlider.value))%"
+    TipInfoLabel.sizeToFit()
+  }
+  
+  func getTipAmount(amount: Double, perc: Double = 0.15) -> (tip: Double, total: Double){
+    let tip = amount * perc
+    return (tip, tip + amount)
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  @IBAction func onBillAmountEditingChanged(sender: AnyObject) {
+    let amount = NSString(string: BillAmount.text).doubleValue
+    let (tip, total) = getTipAmount(amount)
+    let tipString = String(format: "$%.2f", tip)
+    let totalString = String(format: "$%.2f", total)
+    TipLabel.text = tipString
+    TotalLabel.text = totalString
+  }
 
+  @IBAction func onTapOutside(sender: UITapGestureRecognizer) {
+    BillAmount.resignFirstResponder()
+  }
 
+  @IBAction func onSliderValueChanged(sender: AnyObject) {
+    let sliderValue = lroundf(TipSlider.value)
+    TipSlider.setValue(Float(sliderValue), animated: false)
+    TipInfoLabel.text = "Tip @ \(sliderValue)%"
+  }
 }
 
