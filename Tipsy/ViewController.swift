c1a9147094976f14e51e7056a241fcfd04e5237b
Manvesh <manvesh@Manveshs-MacBook-Pro.local>
2014-08-20 01:59:11 -0700

    adding persistence.

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -20,12 +20,41 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var TipSlider: UISlider!
   
+  let billAmountKey = "BillAmount_text"
+  let tipSliderKey = "TipSlider_value"
+  let appCloseTimeKey = "AppCloseTime"
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    BillAmount.text = ""
+    var defaults = NSUserDefaults.standardUserDefaults()
+    if let lastAppCloseTime: AnyObject = defaults.valueForKey(appCloseTimeKey) {
+      if (NSDate.timeIntervalSinceReferenceDate() - (lastAppCloseTime as NSNumber) < 3600.0) {
+        let tipValue = defaults.floatForKey(tipSliderKey)
+        if (tipValue != TipSlider.value) { TipSlider.setValue(tipValue, animated: false) }
+        
+        if let billAmount = defaults.stringForKey(billAmountKey) {
+          BillAmount.text = billAmount as String
+        }
+        self.updateTips()
+      }
+    }
     TipInfoLabel.text = "Tip @ \(lroundf(TipSlider.value))%"
     TipInfoLabel.sizeToFit()
+  }
+  
+  override func viewWillDisappear(animated: Bool) {
+    storeCurrentValues()
+  }
+  
+  func storeCurrentValues() {
+    var defaults = NSUserDefaults.standardUserDefaults()
+    defaults.setObject(BillAmount.text, forKey: billAmountKey)
+    defaults.setFloat(TipSlider.value, forKey: tipSliderKey)
+    let currentDate = NSDate.timeIntervalSinceReferenceDate()
+    defaults.setObject(currentDate, forKey: appCloseTimeKey)
+    defaults.synchronize()
+    println("Completed UserDefaults synchronization")
   }
   
   func getTipAmount(amount: Double, perc: Double = 0.15) -> (tip: Double, total: Double){
@@ -37,13 +66,19 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-  @IBAction func onBillAmountEditingChanged(sender: AnyObject) {
+  
+  func updateTips() {
     let amount = NSString(string: BillAmount.text).doubleValue
-    let (tip, total) = getTipAmount(amount)
+    let (tip, total) = getTipAmount(amount, perc: Double(TipSlider.value * 0.01))
     let tipString = String(format: "$%.2f", tip)
     let totalString = String(format: "$%.2f", total)
     TipLabel.text = tipString
     TotalLabel.text = totalString
+    storeCurrentValues()
+  }
+  
+  @IBAction func onBillAmountEditingChanged(sender: AnyObject) {
+    updateTips()
   }
 
   @IBAction func onTapOutside(sender: UITapGestureRecognizer) {
@@ -51,9 +86,11 @@ class ViewController: UIViewController {
   }
 
   @IBAction func onSliderValueChanged(sender: AnyObject) {
+    BillAmount.resignFirstResponder()
     let sliderValue = lroundf(TipSlider.value)
     TipSlider.setValue(Float(sliderValue), animated: false)
     TipInfoLabel.text = "Tip @ \(sliderValue)%"
+    updateTips()
   }
 }
 
